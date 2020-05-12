eb9dfceafb250d5fac50f6057e4b790672f5ec97
[synnaf] <[f.vforsman@gmail.com]>
2020-05-12 21:46:33 +0200

    Styling for basic component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrintListComponent } from './print-list/print-list.component';
+import { DataListComponent } from './data-list/data-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrintListComponent,
+    DataListComponent
   ],
   imports: [
     BrowserModule,
